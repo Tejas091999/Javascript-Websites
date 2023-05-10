351a6fe2853ef6028d029c3c60f978e37702b1f9
Tejas091999 <tejaslondhe9@gmail.com>
2023-05-10 14:13:30 +0530

    Update script.js

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -6,4 +6,12 @@ button1.addEventListener("click",final)
 function final(){
   var final1= weight1.value/height1.value
   para1.innerHTML = final1 
+  if (final1 < 18.4){
+    para1.innerHTML = "Underweight"}
+  else if (18.4 < final1< 24.9){
+    para1.innerHTML = "Normal"}
+  else if (24.9 < final1 < 40){
+    para1.innerHTML = "OverWeight"}
+  else {
+    para1.innerHTML = "Obese"}
 }
